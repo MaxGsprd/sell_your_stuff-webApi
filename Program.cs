a710d58a39340cf073c5178c34fe880c0a8b308b
MaxGsprd <max.gaspard@outlook.fr>
2023-03-12 17:43:05 +0100

    modify cors allowOrigin

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options => options.AddPolicy(
         name: "sellyourstuffClient",
-        policy => { policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader(); }
+        policy => { policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }
     ));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
